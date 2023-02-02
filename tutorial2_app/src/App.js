c788c45a097215da0f944835bcd090b95b86c1c0
Subham Sharma <sb407046@dal.ca>
2023-02-02 18:18:24 -0400

    tutorial 2 project added

---
+++ tutorial2_app/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>This Project is Working</h1>
+      <p>Subham Sharma - B00910585</p>
+    </div>
+  );
+}
+
+export default App;
